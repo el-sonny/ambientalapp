2034e118ae650b97057cdd668868d810ee6f56cd
sonny <sonny@spaceshiplabs.com>
2014-04-15 19:11:15 -0500

    find coords boilerplate

--- api/controllers/MiaController.js
+++ api/controllers/MiaController.js
@@ -6,5 +6,19 @@
  */
 
 module.exports = {
-	
+	listing : function(req,res){
+		Mia.find({entidad:"Quintana roo"}).limit(100).populate('status').sort('fecha_de_ingreso DESC').exec(function(e,mias){
+			if(e) throw(e);
+			res.view({mias:mias});
+		});
+	},
+	perfil : function(req,res){
+		Mia.findOne({clave:req.param('id')}).populate('status').exec(function(e,mia){
+			if(e) throw(e);
+			res.view({mia:mia});
+		});
+	},	
+	findCoordinates : function(req,res){
+
+	},
 };
--- api/models/Mia.js
+++ api/models/Mia.js
@@ -40,11 +40,24 @@ function file_status(name,mia){
 	var links = [local_file,mia[name],false];
 	var buttons = ['success','info','danger'];
 	var status = mia[name+'_file'] ? 0 : mia[name] ? 1 : 2;
+	var saveButtons = [
+		"<a href='#' class='btn btn-success btn-lg' type='button' disabled='disabled' >"+name+" copiado</a>",
+		"<a href='/scraper/downloadProjectFile/"+mia.clave+"?filetype="+name+"' class='download-doc btn btn-primary btn-lg' type='button' >copiar "+name+"</a>",		
+		"<a href='#' class='btn btn-danger btn-lg' type='button' disabled='disabled'>"+name+" no encontrado</a>",
+	];
+	var openButtons = [
+		"<a href='/mia/findCoordinates/"+mia.clave+"?filetype="+name+"' class='btn btn-success btn-lg' type='button' >abrir "+name+"</a>",
+		"<a href='#' disabled='disabled'  class='btn btn-warning btn-lg' type='button' >guarde el "+name+" primero</a>",		
+		"<a href='#' class='btn btn-danger btn-lg' type='button' disabled='disabled'>"+name+" no encontrado</a>",
+	];
+
 	return {
 		label : labels[status],
 		button : buttons[status],
 		link : links[status],
 		disabled : status == 2 ? "disabled='disabled'" : '',
-		code : status
+		code : status,
+		saveButton : saveButtons[status],
+		openButton : openButtons[status],
 	}
 }
