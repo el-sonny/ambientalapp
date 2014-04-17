4ffc79ad0f4ede32da79ee900883ae9662a37059
sonny <sonny@spaceshiplabs.com>
2014-04-16 22:42:04 -0500

    finish refactor coord search class

--- assets/js/coordinates-finder.js
+++ assets/js/coordinates-finder.js
@@ -11,37 +11,130 @@ function coordinatesFinder(space,text){
 	this.search_space = this.panel.find('pre').html(this.text).addClass('search-space');
 	this.text = text;
 	this.formatSelect = this.panel.find('select').val(space.format);
-	this.activationSwitch = this.panel.find('.space-activation-checkbox').bootstrapSwitch({onText:'si',offText:'no'})
-	.on('switchChange.bootstrapSwitch', 
-	function(e,state){
-		var panel = $(this).parent().parent().parent().parent().parent();
-		//console.log(panel.attr('class'));
-		if(state){
-			panel.find('.workspace').removeClass('disabled');
-		}else{
-			panel.find('.workspace').addClass('disabled');
-		}
-	});
-	this.previewSwitch = this.panel.find('.map-preview-checkbox').bootstrapSwitch({onText:'on',offText:'off'}).on('switchChange.bootstrapSwitch', 
-	function(e,state){
-		var panel = $(this).parent().parent().parent().parent().parent();
-		if(state){
-			panel.find('pre').hide();
-			panel.find('.map').removeClass('hidden');
-			load_map(panel);
-		}else{
-			panel.find('pre').show();
-			panel.find('.map').addClass('hidden');
-		}
+	
+	this.initPoints();
+	this.initCoords();
 
-	});
+	this.panel.find('.space-activation-checkbox').bootstrapSwitch({onText:'si',offText:'no'})
+	.on('switchChange.bootstrapSwitch', $.proxy(this.toggleActivate,this));
 
+	this.panel.find('.map-preview-checkbox').bootstrapSwitch({onText:'on',offText:'off'})
+	.on('switchChange.bootstrapSwitch', $.proxy(this.togglePreview,this));
 
-	$('#parse-space').append(this.panel);
+	if(this.coords.length) $('#parse-space').append(this.panel);
 }
- 
-coordinatesFinder.prototype.getInfo = function(space){
+coordinatesFinder.prototype.toggleActivate = function(e,state){
+	if(state){
+		this.panel.find('.workspace').removeClass('disabled');
+	}else{
+		this.panel.find('.workspace').addClass('disabled');
+	}
+}
+coordinatesFinder.prototype.togglePreview = function(e,state){
+	if(state){
+		this.panel.find('pre').hide();
+		this.panel.find('.map').removeClass('hidden');
+		this.load_map();
+	}else{
+		this.panel.find('pre').show();
+		this.panel.find('.map').addClass('hidden');
+	}
+}
+coordinatesFinder.prototype.load_map = function(){
+	var mapCanvas = this.panel.find('.map').html('');
+	console.log(this.coords);
+	$.post('/mia/convertUTM',{points:this.coords},function(points){
+		console.log(points);
+		var center = get_center(points);
+		var startMapOptions = {
+          center: new google.maps.LatLng(center.x,center.y),
+          zoom: 17,
+          mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
 
+		var map = new google.maps.Map(mapCanvas.get(0), startMapOptions);
+
+		var proyectCoordinates = [];
+		points.forEach(function(point){
+			proyectCoordinates.push(new google.maps.LatLng(point.x,point.y));
+		});
+		proyectCoordinates.push(new google.maps.LatLng(points[0].x,points[0].y))
+
+		var proyectPath = new google.maps.Polygon({
+			paths: proyectCoordinates,
+			geodesic: true,
+			strokeColor: '#eeee22',
+			strokeOpacity: 1.0,
+			strokeWeight: 2,
+			fillColor: '#999912',
+			fillOpacity: .55
+		});
+		proyectPath.setMap(map);
+	},'json');
+	//this.map = map;
+}
+
+coordinatesFinder.prototype.initPoints = function(){
+	var points = [];
+	this.search_space.find('.high').each(function(index){
+		var even = index % 2 == 0;
+		var xy = even ? "x" : "y";
+		var pair = even ? index + 1 : index -1;
+		var point = {
+			index : $(this).index(),
+			component_class : 'coord-' + pair,
+			component_index : pair,
+			value : $(this).html(),
+			xy : even ? "x" : "y",
+			_class : 'coord-'+index,
+			color : even ? 'btn-primary' : 'btn-success' ,
+		}
+		points.push(point);
+
+		$(this).addClass('btn dropdown-toggle')
+		.addClass(point.color)
+		.attr('type','button')
+		.attr('data-toggle','dropdown')
+		.attr('data-xy',point.xy )
+		.attr('data-component',point.component_class)
+		.attr('data-value',point.value)
+		.append(' <span class="caret"></span>')
+		.wrap( "<div class='btn-group'></div>" )
+		.addClass(point._class);
+
+		var xyindex = even ? 0 : 1
+		var check = ' <span class="glyphicon glyphicon-ok"></span>';
+		var menu = $("#sample-menu").clone().attr('id','');
+		menu.children('li').eq(xyindex).children().append(check);
+		$(this).after(menu);
+	});
+	this.points = points;
+}
+
+coordinatesFinder.prototype.initCoords = function(){
+	var pre = this.search_space;
+	var space = this.panel;
+	var row = space.find('.clone-row').detach().removeClass('clone-row');
+	var table = space.find('table');
+	var coords = [];
+	var points = this.points;
+	this.points.forEach(function(point){
+		if(point.xy == 'x'){
+			if(y = points[point.component_index]){
+				var coord = {
+					x : point.value,
+					y : y.value,
+				};
+				coords.push(coord);
+				tds = row.clone();
+				tds.children('td').eq(0).html(coords.length).next().html(coord.x).next().html(coord.y);
+				table.append(tds);
+			}
+		}
+	});
+
+	if(coords.length) space.find('.map-preview-checkbox').bootstrapSwitch('disabled',false);
+	this.coords = coords;
 }
 
 function highlight(text,start,end){
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,106 +10,23 @@ $().ready(function(){
 	});
 
 	$(document).on('mouseenter','.high',function(){
-		$('#'+$(this).attr('data-component')).addClass('active');
+		$('.'+$(this).attr('data-component')).addClass('active');
 	}).on('mouseleave','.high',function(){
-		$('#'+$(this).attr('data-component')).removeClass('active');
+		$('.'+$(this).attr('data-component')).removeClass('active');
 	});
 	
 	$('.find-coordinates').click(function(e){
 		e.preventDefault();
 		var button = $(this).attr('disabled','disabled').html('Buscando coordenadas');
 		$.get(button.attr('href'),function(result){			
-			result.spaces.forEach(function(space){
+			result.spaces.forEach(function(space,index){
 				var finder = new coordinatesFinder(space,result.text);
 			});
-			
-			$('pre .high').each(function(){
-				var index = $(this).index();
-				var even = index % 2 == 0;
-				var component = even ? index + 1 : index -1;
-				var value = $(this).html();
-				component = 'coord-'+component;
-				var color = even ? 'btn-primary' : 'btn-success' ;
-				$(this).addClass('btn dropdown-toggle')
-				.addClass(color)
-				.attr('type','button')
-				.attr('data-toggle','dropdown')
-				.attr('data-xy',even ? "x" : "y" )
-				.attr('data-component',component)
-				.attr('data-value',value)
-				.append(' <span class="caret"></span>')
-				.wrap( "<div class='btn-group'></div>" )
-				.addClass('coord-'+index);
-				var xyindex = even ? 0 : 1
-				var check = ' <span class="glyphicon glyphicon-ok"></span>';
-				var menu = $("#sample-menu").clone().attr('id','');
-				menu.children('li').eq(xyindex).children().append(check);
-				$(this).after(menu);
-			});
-
-			$('pre.search-space').each(function(){
-				var pre = $(this);
-				var space = pre.parent().parent();
-				var row = space.find('.clone-row').detach();
-				var table = space.find('table');
-				var points = [];
-				$(this).find('.high').each(function(){
-					var xy = $(this).attr('data-xy');
-					if(xy == 'x'){
-						var x = $(this).attr('data-value');
-						var y = pre.find('.'+$(this).attr('data-component')).attr('data-value');
-						if(x && y){ 
-							points.push({x:x,y:y});
-							tds = row.clone();
-							tds.children('td').eq(0).html(points.length).next().html(x).next().html(y);
-							table.append(tds);
-						}
-					}
-				})
-				if(points.length){
-					space.find('.map-preview-checkbox').bootstrapSwitch('disabled',false);
-					space.find('.points').html(JSON.stringify(points));
-				}else{
-					pre.parent().parent().detach();
-				};
-			})
-
 			$('html, body').animate({ scrollTop: $('#parse-space').offset().top - 100 }, 'slow');
 		},'json');
 	});
 })
-function load_map(panel){
-	var points = $.parseJSON(panel.find('.points').html());
-	$.get('/mia/convertUTM',{points:points},function(points){
-		var mapCanvas = panel.find('.map').html('');
-		var center = get_center(points);
-		var startMapOptions = {
-          center: new google.maps.LatLng(center.x,center.y),
-          zoom: 17,
-          mapTypeId: google.maps.MapTypeId.SATELLITE
-        };
 
-		var map = new google.maps.Map(mapCanvas.get(0), startMapOptions);
-
-		var proyectCoordinates = [];
-		points.forEach(function(point){
-			proyectCoordinates.push(new google.maps.LatLng(point.x,point.y));
-		});
-		proyectCoordinates.push(new google.maps.LatLng(points[0].x,points[0].y))
-
-		var proyectPath = new google.maps.Polygon({
-			paths: proyectCoordinates,
-			geodesic: true,
-			strokeColor: '#eeee22',
-			strokeOpacity: 1.0,
-			strokeWeight: 2,
-			fillColor: '#999912',
-			fillOpacity: .55
-		});
-
-		proyectPath.setMap(map);
-	},'json');
-}
 function get_center(points){
 	var max = {x:points[0].x,y:points[0].y};
 	var min = {x:points[0].x,y:points[0].y};
